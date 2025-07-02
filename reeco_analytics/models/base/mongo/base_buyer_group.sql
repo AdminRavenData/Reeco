2d3c6c8ce9b148d2027b1adc732a4b51c21bf631
YishaiRaven <165772954+YishaiRaven@users.noreply.github.com>
2025-07-02 16:31:10 +0300

    Update base_buyer_group.sql

--- reeco_analytics/models/base/mongo/base_buyer_group.sql
+++ reeco_analytics/models/base/mongo/base_buyer_group.sql
@@ -1,8 +1,8 @@
 select 
 CHAIN_ID,
 CHAIN_NAME,
-IS_DISABLED,
-IS_DEMO,
+IS_DISABLED_CHAIN,
+IS_DEMO_CHAIN,
 count(distinct BUYER_ID) as count_distinct_buyers
 from
 {{ref("stg_buyer_department_outlet")}}
@@ -10,5 +10,5 @@ from
 group by 
 CHAIN_ID,
 CHAIN_NAME,
-IS_DISABLED,
-IS_DEMO
+IS_DISABLED_CHAIN,
+IS_DEMO_CHAIN
